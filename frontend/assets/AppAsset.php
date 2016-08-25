0ffc1b89cc604d192eb8cee9fb8036d1502de5c1
Grifindorf <Grifindorf1@gmail.com>
2016-08-25 15:40:50 +0300

    New tamplate "startbootstrap-modern-business-gh-pages"

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -16,21 +16,13 @@ class AppAsset extends AssetBundle
 	    '//fonts.googleapis.com/css?family=Kaushan+Script',
 	    '//fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic',
 	    '//fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700',
-	    'css/bootstrap.css',
 	    'css/bootstrap.min.css',
-	    'css/agency.min.css',
+	    'css/modern-business.css',
 	    'css/font-awesome.min.css'
     ];
     public $js = [
-	        'js/jquery.js',
-			'js/jquery.min.js',
-		    'js/agency.min.js',
-		    'js/agency.js',
-	        'js/bootstrap.js',
-			'js/bootstrap.min.js',
-			'js/jqBootstrapValidation.js',
-			'js/contact_me.js'
-
+	    'js/jquery.js',
+		'js/bootstrap.min.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
