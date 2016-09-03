afa82d19ebbc1bcf11bd0647fbf3a80cf479e2f8
Grifindorf <Grifindorf1@gmail.com>
2016-09-03 12:30:51 +0300

    promo

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -158,43 +158,20 @@ AppAsset::register($this);
 <div class="container">
 
 	<!-- Marketing Icons Section -->
-	<div class="row">
-		<div class="col-lg-12">
-			<h1 class="page-header">
-				Welcome to Modern Business
-			</h1>
-		</div>
+	<div id="promo" class="row">
 		<div class="col-md-4">
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h4><i class="fa fa-fw fa-check"></i> Bootstrap v3.3.7</h4>
-				</div>
-				<div class="panel-body">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-					<a href="#" class="btn btn-default">Learn More</a>
-				</div>
+			<div class="panel-heading">
+				<span class="promo-blocks"><i class="fi fi-fw fi-cert"></i>Официальный представитель Forwerk (Германия) в Украине </span>
 			</div>
 		</div>
 		<div class="col-md-4">
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h4><i class="fa fa-fw fa-gift"></i> Free &amp; Open Source</h4>
-				</div>
-				<div class="panel-body">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-					<a href="#" class="btn btn-default">Learn More</a>
-				</div>
+			<div class="panel-heading">
+				<span class="promo-blocks"><i class="fi fi-fw fi-hand-shake"></i> Работа напряму с производителем</span>
 			</div>
 		</div>
 		<div class="col-md-4">
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h4><i class="fa fa-fw fa-compass"></i> Easy to Use</h4>
-				</div>
-				<div class="panel-body">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-					<a href="#" class="btn btn-default">Learn More</a>
-				</div>
+			<div class="panel-heading">
+				<span class="promo-blocks"><i class="fi fi-fw fi-price-policy"></i>Гибкая ценовая политика </span>
 			</div>
 		</div>
 	</div>
@@ -326,7 +303,7 @@ AppAsset::register($this);
 			<div class="row">
 				<div class="col-md-4">
 					<span class="copyright">citinox.com.ua © 2016</span>
-					<span class="copyright-mid">Мы предлагаем большой выбор высококачественных креплений для изделий из стекла и табличек производства компании FORWERK (Германия)</span>
+					<div class="copyright-mid">Мы предлагаем большой выбор высококачественных креплений для изделий из стекла и табличек производства компании FORWERK (Германия)</div>
 					<span class="de">citinox.de</span>
 				</div>
 				<div class="col-md-4">
